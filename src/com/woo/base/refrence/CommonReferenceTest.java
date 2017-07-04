993d4a28e32c47741b4a87043e1cc36395d57cb9
windgoogle <windgoogle@aliyun.com>
2017-07-04 10:08:50 +0800

    JDK7  Fork-Join Demo

---
+++ src/com/woo/base/refrence/CommonReferenceTest.java
@@ -0,0 +1,46 @@
+package com.woo.base.refrence;
+
+/**
+ * Created by huangfeng on 2017/7/4.
+ */
+public class CommonReferenceTest {
+    private static Person person;
+
+    public static void main(String[] args) throws InterruptedException {
+        person = new Person("Tom", 18);
+
+        System.out.println("Person is " + person.toString());
+
+        Thread t1 = new Thread(new Task1());
+        Thread t2 = new Thread(new Task2());
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.out.println("Now Person is " + person.toString());
+    }
+
+    static class Task1 implements Runnable {
+        public void run() {
+            person.setAge(person.getAge() + 1);
+            person.setName("Tom1");
+
+            System.out.println("Thread1 Values "
+                    + person.toString());
+        }
+    }
+
+    static class Task2 implements Runnable {
+        public void run() {
+            person.setAge(person.getAge() + 2);
+            person.setName("Tom2");
+
+            System.out.println("Thread2 Values "
+                    + person.toString());
+        }
+    }
+
+}
