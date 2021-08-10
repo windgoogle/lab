60c9370d82720bb604286e6cb595b9ce600122ff
huangfeng <huangfeng@tongtech.com>
2021-08-10 16:38:39 +0800

    补充提交

---
+++ src/com/woo/base/BigNum.java
@@ -0,0 +1,13 @@
+package com.woo.base;
+
+public class BigNum {
+
+    public static void main(String[] args) {
+        long l=100_000_00;
+        if(l==100000) {
+            System.out.println("------------"+l);
+        }else{
+            System.out.println("------------"+false+"  l="+l);
+        }
+    }
+}
