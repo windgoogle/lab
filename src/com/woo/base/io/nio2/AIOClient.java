a44b71cdef060e192e56d8b5c14abc8666996fb5
huangfeng <huangfeng@tongtech.com>
2022-06-30 17:15:27 +0800

    AIO demo

---
+++ src/com/woo/base/io/nio2/AIOClient.java
@@ -0,0 +1,13 @@
+package com.woo.base.io.nio2;
+
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousSocketChannel;
+
+public class AIOClient {
+    public static void main(String... args) throws Exception {
+        AsynchronousSocketChannel client = AsynchronousSocketChannel.open();
+        client.connect(new InetSocketAddress("localhost", 9888));
+        client.write(ByteBuffer.wrap("test".getBytes())).get();
+    }
+}
